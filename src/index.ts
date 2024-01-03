3f7c565460347b2cdb5f1e9ca3472a13f71092d0
Fernanda Moscoso <carolina.moscosot@gmail.com>
2024-01-03 00:03:13 -0500

    Practice

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
- 
+
 const app = express();
 const port = 3000;
- 
+
+// ROUTING
+
+// METOD (GET, POST, PATH, DELETE, PUT ...) + PATH (dominio.com/note/10?special=true)
 app.get("/", (req, res) => {
+    // manejar solicitud GET a '/'
+    // Lógica para manejar una solicitud GET a '/users'
   console.log(`${req.method} ${req.url}`);
-  res.send("Hola Mundo con Express y TypeScript!");
+  //   res.send("Hola Mundo con Express y TypeScript!");
+  // Respuesta formato JSON
+  res.json({ res: "Hola mundo" });
 });
- 
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
